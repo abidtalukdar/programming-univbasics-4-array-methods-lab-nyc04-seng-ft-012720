c839f4e9ba4e773b2ab8309c30f6ffa22d5fb9ce
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-13 20:50:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,17 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  arr_sorted = array.sort!
 end
 
 def using_reverse(array)
-
+  arr_reversed = array.reverse
 end
 
 def using_first(array)
-
+  
 end
 
 def using_last(array)
